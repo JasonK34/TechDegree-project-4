c154ad823f444ad1af4df7a03d04e5b2dd9d8191
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-12-26 06:14:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 # Techdegree Project 4
 
-import pewee
+from pewee import *
+import os
+
+
+db = SqliteDatabase('inventory.db')
+
+class Product(Model):
+    product_id = 
+    product_name = 
+    product_quantity = 
+    product_price = 
+    date_updated = 
+
+    class Meta:
+        database = db
