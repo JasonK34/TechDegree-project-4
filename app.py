4aa2fa6de49b38deb87811b8eabc80a9a6b7554c
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-12-29 21:04:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 # Techdegree Project 4
 
-from pewee import *
+from peewee import *
 import os
+import datetime
 
 
 db = SqliteDatabase('inventory.db')
 
 class Product(Model):
-    product_id = 
-    product_name = 
-    product_quantity = 
-    product_price = 
-    date_updated = 
+    product_id = CharField(primary_key=True)
+    product_name = TextField()
+    product_quantity = IntegerField()
+    product_price = IntegerField()
+    date_updated = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
+    
+        
+        
+if __name__ == "__main__":
+    db.connect()
+    
