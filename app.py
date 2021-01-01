421973e7905fcf6a7523e7d5a9d411224fd59174
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-12-31 23:58:19 -0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Techdegree Project 4
 
 from peewee import *
-import os
 import datetime
 import csv
 
@@ -9,8 +8,8 @@ import csv
 db = SqliteDatabase('inventory.db')
 
 class Product(Model):
-    product_id = CharField(primary_key=True)
-    product_name = TextField()
+    product_id = PrimaryKeyField()
+    product_name = TextField(unique=True)
     product_quantity = IntegerField()
     product_price = IntegerField()
     date_updated = DateTimeField(default=datetime.datetime.now)
@@ -18,12 +17,23 @@ class Product(Model):
     class Meta:
         database = db
     
-with open('inventory.csv', newline= '') as csvfile:
-    products = csv.DictReader(csvfile)        
-    rows = list(products)
-    for row in rows[1:3]:
-        print(', '.join(row))  
+def inventory():
+    with open('inventory.csv', newline='') as csvfile:
+        products = csv.DictReader(csvfile)
+        rows = list(products)
+        for row in rows:
+            for key, value in row.items():
+                if key == "product_quantity":
+                    product_quantity = int(value)
+                if key == "product_price":
+                    product_price = float(value.replace("$", ""))
+                if key == "date_updated":
+                    date_updated = datetime.datetime
+
+        print(row)
 
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Product], safe=True)
+    inventory()
+
