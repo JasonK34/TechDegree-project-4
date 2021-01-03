2892098fa49e064b665a2c1bd9d97ba163ec1c12
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2021-01-02 22:38:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -36,15 +36,37 @@ def inventory():
                     product_price_list.append(value)
 
                 if key == "date_updated":
+                    value = datetime.datetime.strptime(value, "%m/%d/%Y")
+                    date_updated_list.append(value)
                     print(value)
-                    value = datetime.datetime.strptime(value, "%m %d %Y")
-                    print(value)
+
+def add_data():
+    # add the products listed in the inventory file to the database
+
+def menu():
+    print("Welcome to the Inventory! \n")
+    options = input("Enter a selection: \n",
+                    "'v' to view details of a product\n"
+                    "'a' to add product\n"
+                    "'b' to create a backup\n")
+
+def product_id():
+    # get and display a product by its product_id
+
+def add_product():
+    # add a product to the database; prompt user to enter the product's name, quantity, and price
+    # process the entered price from a string to an integer (convert to cents)
+
+def back_up():
+    # makes back_up of the database and writes it to a .csv file
+
 
 
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Product], safe=True)
     inventory()
+    menu()
 
 
 
