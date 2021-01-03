2bf4ad740c7013806abebcf9b19105ff3f6bf027
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2021-01-02 23:22:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from peewee import *
 import datetime
 import csv
+import sys
 
 
 db = SqliteDatabase('inventory.db')
@@ -38,26 +39,50 @@ def inventory():
                 if key == "date_updated":
                     value = datetime.datetime.strptime(value, "%m/%d/%Y")
                     date_updated_list.append(value)
-                    print(value)
 
-def add_data():
+#def add_data():
     # add the products listed in the inventory file to the database
 
+
 def menu():
-    print("Welcome to the Inventory! \n")
-    options = input("Enter a selection: \n",
+    print("\nWelcome to the Inventory! \n")
+    options = input("Enter a selection:\n\n"
                     "'v' to view details of a product\n"
                     "'a' to add product\n"
-                    "'b' to create a backup\n")
+                    "'b' to create a backup\n"
+                    "'q' to quit\n\n"
+                    "---> ").lower().strip()
 
-def product_id():
+    if options == "v":
+        product_id()
+
+    if options == "a":
+        add_product()
+
+    if options == "b":
+        back_up()
+
+    if options == 'q':
+        print("Goodbye")
+
+
+
+#def product_id():
     # get and display a product by its product_id
 
 def add_product():
     # add a product to the database; prompt user to enter the product's name, quantity, and price
     # process the entered price from a string to an integer (convert to cents)
+    print("Enter product name: ")
+    item = sys.stdin.read().strip()
 
-def back_up():
+    if item:
+        if input("Save product? 'y' ").lower() != 'n':
+            Product.create(content=item)
+            print("Saved!")
+
+
+#def back_up():
     # makes back_up of the database and writes it to a .csv file
 
 
