d0737763d8de61356190b57410d459ffa5876317
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2021-01-09 20:01:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 # Techdegree Project 4
 
-from peewee import *
+from collections import OrderedDict
 import datetime
 import csv
 import sys
 
+from peewee import *
+
 
 db = SqliteDatabase('inventory.db')
 
-product_quantity_list = []
-product_price_list = []
-date_updated_list = []
-
 class Product(Model):
     product_id = PrimaryKeyField()
-    product_name = TextField(unique=True)
+    product_name = CharField(max_length=255, unique=True)
     product_quantity = IntegerField()
     product_price = IntegerField()
     date_updated = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
+
+def initialize():
+    db.connect()
+    db.create_tables([Product], safe=True)
 
 def inventory():
     with open('inventory.csv', newline='') as csvfile:
@@ -30,68 +32,67 @@ def inventory():
             for key, value in row.items():
                 if key == "product_quantity":
                     value = int(value)
-                    product_quantity_list.append(value)
 
                 if key == "product_price":
                     value = int(float(value.replace("$", "")) * 100)
-                    product_price_list.append(value)
 
                 if key == "date_updated":
                     value = datetime.datetime.strptime(value, "%m/%d/%Y")
-                    date_updated_list.append(value)
-
-#def add_data():
-    # add the products listed in the inventory file to the database
-
-
-def menu():
-    print("\nWelcome to the Inventory! \n")
-    options = input("Enter a selection:\n\n"
-                    "'v' to view details of a product\n"
-                    "'a' to add product\n"
-                    "'b' to create a backup\n"
-                    "'q' to quit\n\n"
-                    "---> ").lower().strip()
-
-    if options == "v":
-        product_id()
-
-    if options == "a":
-        add_product()
-
-    if options == "b":
-        back_up()
-
-    if options == 'q':
-        print("Goodbye")
 
 
 
-#def product_id():
-    # get and display a product by its product_id
+def menu_loop():
+    choice = None
+
+    while choice != 'q':
+        print("Enter 'q' to quit")
+        for key, value in menu.items():
+            print('{}) {}'.format(key, value.__doc__))
+        choice = input('Enter choice: ').lower().strip()
+
+        if choice in menu:
+            menu[choice]()
+
+
+def view_product(view_product=None):
+    #get and display a product by its product_id
+    """View Product from Inventory"""
+    view_product = input( 'Name of product: ' )
+    items = Product.select().order_by(Product.product_name.asc())
+    if view_product:
+       items = items.where(Product.product_name.contains(view_product))
+
+    for item in items:
+        print(item.product_name)
+
 
 def add_product():
     # add a product to the database; prompt user to enter the product's name, quantity, and price
     # process the entered price from a string to an integer (convert to cents)
-    print("Enter product name: ")
+    """Add Product to Inventory"""
+    print("Enter product name. Press 'command+d' when finished.\n")
     item = sys.stdin.read().strip()
 
     if item:
-        if input("Save product? 'y' ").lower() != 'n':
+        if input("Save product? [Yn] ").lower() != 'n':
             Product.create(content=item)
-            print("Saved!")
+            print("Product Saved!")
 
 
-#def back_up():
-    # makes back_up of the database and writes it to a .csv file
+def backup():
+    #makes back_up of the database and writes it to a .csv file
+    """Backup Inventory"""
 
-
+menu = OrderedDict([
+    ('v', view_product),
+    ('a', add_product),
+    ('b', backup),
+])
 
 if __name__ == "__main__":
-    db.connect()
-    db.create_tables([Product], safe=True)
+    initialize()
     inventory()
-    menu()
+    menu_loop()
 
 
 
