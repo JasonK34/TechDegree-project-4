85c990f2997b7cf466113aab5f0b1c59f3f4f0c4
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2021-01-18 21:00:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def inventory():
             row['product_quantity'] = int(row['product_quantity'])
             row['product_price'] = int(float(row['product_price'].replace("$", ""))) * 100
             row['date_updated'] = datetime.datetime.strptime(row['date_updated'], "%m/%d/%Y")
-            print(row)
+            
 
 def menu_loop():
     choice = None
